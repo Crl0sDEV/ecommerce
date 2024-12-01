9f10a06be618d96c33d44c941f07d43a679eb551
Crl0sDEV <sandrinocarlosmiguel@gmail.com>
2024-12-01 16:57:38 +0800

    add  more products

--- src/data/product.js
+++ src/data/product.js
@@ -2,6 +2,9 @@ const products = [
     { id: 1, name: 'Product 1', price: '$29.99', image: 'url1', description: 'Description 1' },
     { id: 2, name: 'Product 2', price: '$39.99', image: 'url2', description: 'Description 2' },
     { id: 3, name: 'Product 3', price: '$49.99', image: 'url3', description: 'Description 3' },
+    { id: 4, name: 'Product 4', price: '$59.99', image: 'url4', description: 'Description 4' },
+    { id: 5, name: 'Product 5', price: '$69.99', image: 'url5', description: 'Description 5' },
+    { id: 6, name: 'Product 6', price: '$79.99', image: 'url6', description: 'Description 6' },
   ];
   
   export default products;  
